2b26af4c9207e36dbd15072be6847780ff9e8058
Amar Omerika <amar.omerika@edu.fit.ba>
2021-04-28 23:43:57 +0200

    KeeperApp

---
+++ src/Note.js
@@ -0,0 +1,17 @@
+const notes = [
+	{
+		key: 1,
+		title: "Delegation",
+		content: " Testing testing testing-...",
+	},
+	{
+		key: 2,
+		title: "Loops",
+		content: " Doing something ",
+	},
+	{
+		key: 3,
+		titel: "Arrays",
+		contetn: " Why am i doing this",
+	},
+];
