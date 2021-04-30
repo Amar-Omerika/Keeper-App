e831f11733fa02727501408ba10975322068f687
Amar Omerika <amar.omerika@edu.fit.ba>
2021-04-30 17:31:03 +0200

    KeeperApp

--- src/notes.js
+++ src/notes.js
@@ -2,17 +2,20 @@ const notes = [
 	{
 		key: 1,
 		title: "Delegation",
-		content: " Testing testing testing-...",
+		content:
+			" Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
 	},
 	{
 		key: 2,
 		title: "Loops",
-		content: " Doing something ",
+		content:
+			" Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
 	},
 	{
 		key: 3,
-		title: "Loops",
-		content: " Doing something ",
+		title: "Array",
+		content:
+			"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
 	},
 ];
 
