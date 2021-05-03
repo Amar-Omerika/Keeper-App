1ece8d64518082b32e4ff2b8cce813fae6230f47
Amar Omerika <amar.omerika@edu.fit.ba>
2021-05-03 22:40:06 +0200

    KeeperApp

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
-function CreateArea() {
+
+function CreateArea(props) {
 	const [note, setNote] = useState({
 		title: "",
 		content: "",
 	});
+
 	function handleChange(event) {
 		const { name, value } = event.target;
+
 		setNote((prevNote) => {
 			return {
 				...prevNote,
@@ -13,26 +16,36 @@ function CreateArea() {
 			};
 		});
 	}
+
+	function submitNote(event) {
+		props.onAdd(note);
+		setNote({
+			title: "",
+			content: "",
+		});
+		event.preventDefault();
+	}
+
 	return (
 		<div>
 			<form>
 				<input
+					name="title"
 					onChange={handleChange}
 					value={note.title}
 					placeholder="Title"
-					name="title"
 				/>
 				<textarea
+					name="content"
 					onChange={handleChange}
 					value={note.content}
-					name="content"
 					placeholder="Take a note..."
-					cols="30"
-					rows="10"
-				></textarea>
-				<button>Add</button>
+					rows="3"
+				/>
+				<button onClick={submitNote}>Add</button>
 			</form>
 		</div>
 	);
 }
+
 export default CreateArea;
